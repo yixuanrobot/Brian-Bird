c2e6339752627934109630e897fb466f5c011a88
canela7 <canelabrian123@gmail.com>
2017-01-29 13:05:12 -0800

    add Bird class and bird assest
    add Bird class and bird assest

---
+++ Bird.swift
@@ -0,0 +1,21 @@
+//
+//  Bird.swift
+//  Brian Bird
+//
+//  Created by Brian Canela on 1/29/17.
+//  Copyright © 2017 CanelaApps. All rights reserved.
+//
+
+import SpriteKit
+
+class Bird: SKSpriteNode {
+
+    func initalize() {
+        self.name = "Bird"
+        self.zPosition = 3;
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+    }
+
+
+
+}
--- GameplayScene.swift
+++ GameplayScene.swift
@@ -10,13 +10,27 @@ import SpriteKit
 
 class GameplayScene: SKScene {
 
+    var bird = Bird();
+    
     override func didMove(to view: SKView) {
         initialize();
     }
     
+    override func update(_ currentTime: TimeInterval) {
+        moveBackgroundsAndGrounds();
+    }
+    
     func initialize(){
+        createBird()
         createBackground();
         createGrounds();
+    }
+    
+    func createBird() {
+        bird = Bird(imageNamed: "Blue 1");//we can use imagedName since Bird inherientence from skspritenode\
+        bird.initalize();
+        bird.position = CGPoint(x: -50, y: 0)
+        self.addChild(bird)
     }
     
     func createBackground(){
@@ -40,10 +54,32 @@ class GameplayScene: SKScene {
             ground.anchorPoint = CGPoint(x:0.5, y: 0.5)
             ground.position = CGPoint(x: CGFloat(i) * ground.size.width, y: -(self.frame.size.height / 2));
             self.addChild(ground)
-        
-        
         }
+    }
     
+    func moveBackgroundsAndGrounds(){
+         //this code will move every node that has the name BG by 4.5 to the left side
+        enumerateChildNodes(withName: "BG", using: ({
+            (node, error) in
+            
+            node.position.x -= 4.5;
+            if node.position.x < -(self.frame.width){ //if the node of bg is out of scene
+                node.position.x += self.frame.width * 3; //the node will re do for the scene can not go out of screen, we multiply by 3 because we have 3 bG's
+            }
+            
+        }));
+        //this code will move every node that has the name Ground by 4.5 to the left side
+        enumerateChildNodes(withName: "Ground", using: ({
+            (node, error) in
+            
+            node.position.x -= 2;
+            if node.position.x < -(self.frame.width){ //if the node of gound is out of scene
+                node.position.x += self.frame.width * 3; //the node will re do for the scene can not go out of screen, we multiply by 3 because we have 3 ground
+            }
+            
+        }));
+        
+        
     }
 
 }
